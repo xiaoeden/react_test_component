7ff4e047d024301ab8150f3ddaa9a84bbbe1c890
xiaoeden <linxiaogan.eden@gmail.com>
2022-01-06 13:25:05 -0800

    work with states and events

---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,19 @@
+import "./NewExpenses.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpenses(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+}
+
+export default NewExpenses;
